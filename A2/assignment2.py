e9b62d6090d9ed7b066d864a60b7f42dfc70d1af
Nixon <nixonwidjaja23@gmail.com>
2024-03-10 21:14:41 +0800

    model

--- A2/assignment2.py
+++ A2/assignment2.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python.
+# !/usr/bin/env python.
 
 """
 CS4248 ASSIGNMENT 2 Template
@@ -11,20 +11,74 @@ reference your code with your writeup.
 # Import libraries.  Add any additional ones here.
 # Generally, system libraries precede others.
 import pandas as pd
-from sklearn.metrics import f1_score
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from sklearn.metrics import f1_score, make_scorer
+from sklearn.model_selection import GridSearchCV
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.neural_network import MLPClassifier
 
 # TODO: Replace with your Student Number
 _STUDENT_NUM = "A0236430N"
 
 
+def build_model(model):
+    print(model)
+    f1_macro = make_scorer(f1_score, average="macro")
+    if model == "NB":
+        parameters = {
+            "alpha": [0.01, 0.1, 0.5, 1.0, 5.0, 10.0],
+            "fit_prior": [True, False],
+        }
+        return GridSearchCV(
+            MultinomialNB(), parameters, cv=5, scoring=f1_macro, n_jobs=-1
+        )
+        # return MultinomialNB()
+    elif model == "LR":
+        param_grid = {
+            "C": [1, 10, 100],
+        }
+        logreg = LogisticRegression(
+            max_iter=100,
+            solver="liblinear",
+            penalty="l2",
+            class_weight="balanced",
+        )
+        # return logreg
+        return GridSearchCV(logreg, param_grid, cv=5, scoring=f1_macro, n_jobs=-1)
+    elif model == "NN":
+        param_grid = {"hidden_layer_sizes": [(50, 50)]}
+        classifier = MLPClassifier(
+            max_iter=200,
+            activation="relu",
+            solver="adam",
+            random_state=1,
+            early_stopping=True,
+        )
+        return GridSearchCV(classifier, param_grid, cv=5, scoring=f1_macro, n_jobs=-1)
+
+
+def preprocessing(text: str):
+    # stops = set(stopwords.words("english"))
+    wnl = WordNetLemmatizer()
+
+    text = word_tokenize(text.lower())
+    cleaned = [w for w in text if w.isalpha()]
+    lemmatized = [wnl.lemmatize(w) for w in cleaned]
+    return " ".join(lemmatized)
+
+
 def train_model(model, X_train, y_train):
     """TODO: train your model based on the training data"""
-    pass
+    model.fit(X_train, y_train)
 
 
 def predict(model, X_test):
     """TODO: make your prediction here"""
-    return [0] * len(X_test)
+    return model.predict(X_test)
 
 
 def generate_result(test, y_pred, filename):
@@ -39,9 +93,33 @@ def main():
     train = pd.read_csv("train.csv")
     X_train = train["Text"]
     y_train = train["Verdict"]
-    model = None  # TODO: Define your model here
+
+    test = pd.read_csv("test.csv")
+    X_test = test["Text"]
+
+    model = build_model("NN")  # TODO: Define your model here
+
+    # count_v = CountVectorizer(preprocessor=preprocessing, ngram_range=(1, 2))
+    # X_train = count_v.fit_transform(X_train).toarray()
+    # X_test = count_v.transform(X_test)
+
+    tfidf = TfidfVectorizer(
+        preprocessor=preprocessing,
+        use_idf=True,
+        smooth_idf=True,
+        ngram_range=(1, 2),
+    )
+    X_train = tfidf.fit_transform(X_train)
+    X_test = tfidf.transform(X_test)
+
+    # import spacy
+
+    # nlp = spacy.load("en_core_web_sm")
+    # doc = nlp(text)
 
     train_model(model, X_train, y_train)
+    print(model.best_estimator_)
+    print(model.best_params_)
     # test your model
     y_pred = predict(model, X_train)
 
@@ -50,8 +128,6 @@ def main():
     print("score on validation = {}".format(score))
 
     # generate prediction on test data
-    test = pd.read_csv("test.csv")
-    X_test = test["Text"]
     y_pred = predict(model, X_test)
     generate_result(test, y_pred, _STUDENT_NUM + ".csv")
 
